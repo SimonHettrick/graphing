0c7ad00f2b3715fbe7bf39f03d1f000b00d2eb0e
simonhettrick <s.hettrick@software.ac.uk>
2019-08-13 13:18:22 +0100

    I am sick of having to change the same parameter, title size say, across tons of csvs, so I wrote change_parameter to do it for me. It is not sophisticated, but it works.

--- chart_details_lookup.py
+++ chart_details_lookup.py
@@ -6,8 +6,8 @@ global_specs sets up some global variables for the plots
 '''
 
 global_specs = {
-    'plot_width': 10,
-    'plot_height': 7.2,
-    'dpi': 300,
+    'plot_width': 5,
+    'plot_height': 3,
+    'dpi': 150,
     'font_name': 'Serif'
     }
